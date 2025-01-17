a42a26c3f09a956b1913191594b24fec2a3db601
William Newbery <wnewbery@hotmail.co.uk>
2025-01-17 20:46:21 +0000

    Create hello world project.

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+
+const container = document.getElementById("app")!;
+const root = createRoot(container);
+
+const App = () => {
+  return <h1>Hello, World!</h1>;
+};
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Path to your index.html file
+    }),
+  ],
+  devServer: {
+    port: 9000,
+  },
+  mode: 'development',
+};
