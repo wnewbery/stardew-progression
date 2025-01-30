87901dbbf4360043e31188e1b7ce30db67872166
William Newbery <wnewbery@hotmail.co.uk>
2025-01-30 22:32:25 +0000

    Upgrade to tailwind 4.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,3 @@
-const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
-
 module.exports = {
-  plugins: [tailwindcss("./tailwind.config.cjs"), autoprefixer],
+  plugins: {"@tailwindcss/postcss": {}},
 };
