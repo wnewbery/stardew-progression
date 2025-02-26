c33c0de761e6025fcb99f5f4adbd97a7485be854
wnewbery <wnewbery@hotmail.co.uk>
2025-02-26 13:03:17 +0000

    Improve building info layout to be more mobile friendly.

--- src/components/Building.tsx
+++ src/components/Building.tsx
@@ -1,5 +1,6 @@
 import GameData from "../data/GameData";
 import GameItem from "../data/GameItem";
+import GoldIconSrc from "../assets/20px-Gold.png";
 import { useAppSelector } from "../redux/Store";
 import { setCompleted, setUiElementHidden } from "../redux/ChecklistSlice";
 import { useAppDispatch } from "../redux/Store";
@@ -15,36 +16,70 @@ export default function Building({ id, number }: BuildingProps) {
   const completionId = number ? `${id}_${number}` : id;
   const label = number ? `${building.label} ${number}` : building.label;
 
-  const completed = useAppSelector(state => state.checklist.items[completionId] ?? false);
-  const expanded = useAppSelector(state => !state.checklist.uiElementsHidden[building.id]);
+  const completed = useAppSelector(
+    (state) => state.checklist.items[completionId] ?? false
+  );
+  const expanded = useAppSelector(
+    (state) => !state.checklist.uiElementsHidden[building.id]
+  );
   const dispatch = useAppDispatch();
   const setBuildingOpen = (evt: React.ToggleEvent<HTMLDetailsElement>) => {
-    dispatch(setUiElementHidden({ id: completionId, hidden: !evt.currentTarget.open }));
-  }
+    dispatch(
+      setUiElementHidden({ id: completionId, hidden: !evt.currentTarget.open })
+    );
+  };
   const onCompleted = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(setCompleted({ id: completionId, completed: evt.currentTarget.checked }));
-  }
+    dispatch(
+      setCompleted({ id: completionId, completed: evt.currentTarget.checked })
+    );
+  };
   const hasMaterials = building.materials.length > 0;
 
   return (
-    <details className="border rounded-xl bg-primary border-secondary max-w-xl" open={expanded} onToggle={setBuildingOpen}>
+    <details
+      className="border rounded-xl bg-primary border-secondary max-w-xl"
+      open={expanded}
+      onToggle={setBuildingOpen}
+    >
       <summary className="text-xl font-bold space-x-4 ml-4 h-8">
-        <input name={completionId} type="checkbox" checked={completed} onChange={onCompleted} className="w-6 h-6 align-middle ml-4" />
-        <a className="align-middle" href={building.wiki} target="_blank">{label}</a>
+        <input
+          name={completionId}
+          type="checkbox"
+          checked={completed}
+          onChange={onCompleted}
+          className="w-6 h-6 align-middle ml-4"
+        />
+        <a className="align-middle" href={building.wiki} target="_blank">
+          {label}
+        </a>
       </summary>
 
       <hr className="border-slate-400 mt-4" />
       <div className="flex flex-row items-start space-x-8 p-4">
         <img src={building.icon} alt={building.label} width={96} height={96} />
-        <div className="grid grid-cols-2 items-start">
-          <div className="font-bold w-32">Cost:</div>
-          <div>{building.cost.toLocaleString('en-US')}g</div>
+        <div>
+          <div>
+            <img
+              src={GoldIconSrc}
+              alt="Gold"
+              width={24}
+              height={24}
+              className="inline"
+            />{" "}
+            {building.cost.toLocaleString("en-US")}g
+          </div>
           {hasMaterials && (
             <>
-              <div className="font-bold w-32">Materials:</div>
-              <div>{building.materials.map((material: { item: GameItem, count: number }) => (
-                <ItemStackText className="block" key={material.item.id} item={material.item} count={material.count} />
-              ))}</div>
+              {building.materials.map(
+                (material: { item: GameItem; count: number }) => (
+                  <ItemStackText
+                    className="block"
+                    key={material.item.id}
+                    item={material.item}
+                    count={material.count}
+                  />
+                )
+              )}
             </>
           )}
         </div>
